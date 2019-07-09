cef925ca8be1e2b96c9dd491d13ea1ea7ef6dc62
shimj <mjshi94@gmail.com>
2019-07-09 18:32:40 +0800

    dynamically get the url of the fanyi.min.js

--- update_passcode.py
+++ update_passcode.py
@@ -6,7 +6,6 @@ current_file_path = os.path.abspath(inspect.getfile(inspect.currentframe()))
 base_path = os.path.dirname(current_file_path)
 os.chdir(base_path)
 
-url = "http://shared.ydstatic.com/fanyi/newweb/v1.0.17/scripts/newweb/fanyi.min.js"
 headers = {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15",
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -17,8 +16,13 @@ headers = {
     'Referer': "http://fanyi.youdao.com/",
 }
 
+main_url = "http://fanyi.youdao.com"
+resp = requests.get(url=main_url)
+html = resp.text
 
-resp = requests.get(url=url)
+js_url = re.findall(r"\"([^\"]+fanyi\.min\.js)\"", html)[0]
+
+resp = requests.get(url=js_url)
 js_data = resp.text
 result = re.findall(r"sign:\s?n\.md5[^\)]+\"([^\"]*)\"\)", js_data)[0]
 with open("./passcode.txt", "w") as f:
